5a62a3996b7c0872b64e866c3161024283ba4a31
redron <redronexe@gmail.com>
2026-02-23 01:31:07 +0300

    Dependencies fixed

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,35 +1,10 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
 }
 
-android {
-    namespace = "redron.tradox.core.network"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+kotlin {
+    jvmToolchain(11)
 }
 
 dependencies {
--- core/network/src/main/java/redron/tradox/core/network/api/YahooApi.kt
+++
@@ -1,2 +0,0 @@
-package redron.tradox.core.network.api
-
